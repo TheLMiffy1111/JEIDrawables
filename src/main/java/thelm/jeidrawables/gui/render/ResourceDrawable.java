c48a0e66acb38ba421dbe74d0461ae4e3a1722b2
TheLMiffy1111 <6522061+TheLMiffy1111@users.noreply.github.com>
2025-07-30 02:36:25 +0800

    Fix ResourceDrawable using height as width when drawing

--- src/main/java/thelm/jeidrawables/gui/render/ResourceDrawable.java
+++ src/main/java/thelm/jeidrawables/gui/render/ResourceDrawable.java
@@ -22,7 +22,7 @@ public record ResourceDrawable(ResourceLocation atlasLocation, int u, int v, int
 	@Override
 	public void draw(GuiGraphics guiGraphics, float xOffset, float yOffset, float maskTop, float maskBottom, float maskLeft, float maskRight) {
 		if(maskLeft + maskRight < width && maskTop + maskBottom < height) {
-			GuiRenderUtil.blit(guiGraphics, atlasLocation, xOffset + maskLeft, yOffset + maskTop, u + maskLeft, v + maskTop, width - maskLeft - maskRight, height - maskTop - maskBottom, textureHeight, textureHeight);
+			GuiRenderUtil.blit(guiGraphics, atlasLocation, xOffset + maskLeft, yOffset + maskTop, u + maskLeft, v + maskTop, width - maskLeft - maskRight, height - maskTop - maskBottom, textureWidth, textureHeight);
 		}
 	}
 
