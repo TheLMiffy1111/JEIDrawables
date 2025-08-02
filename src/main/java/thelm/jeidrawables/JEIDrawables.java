e3587d1ee34a191b0223dabd9eeafada0faff728
TheLMiffy1111 <6522061+TheLMiffy1111@users.noreply.github.com>
2025-08-02 16:47:41 +0800

    Add util to append Fabric fractional units to fluid tooltips

--- src/main/java/thelm/jeidrawables/JEIDrawables.java
+++ src/main/java/thelm/jeidrawables/JEIDrawables.java
@@ -2,8 +2,11 @@ package thelm.jeidrawables;
 
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.gui.drawable.IDrawable;
+import mezz.jei.api.gui.ingredient.IRecipeSlotTooltipCallback;
 import mezz.jei.api.runtime.IIngredientManager;
 import mezz.jei.api.runtime.IJeiRuntime;
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.resources.ResourceLocation;
 import thelm.jeidrawables.gui.render.AnimatedDrawable;
 import thelm.jeidrawables.gui.render.DownscaledDrawable;
@@ -61,5 +64,18 @@ public class JEIDrawables implements IModPlugin {
 		else {
 			return FLAME;
 		}
+	}
+
+	public static IRecipeSlotTooltipCallback appendFraction(long fraction) {
+		return (recipeSlotView, tooltip) -> {
+			if(fraction > 0) {
+				for(int i = 0; i < tooltip.size(); ++i) {
+					if(tooltip.get(i) instanceof TranslatableComponent component && "jei.tooltip.liquid.amount".equals(component.getKey())) {
+						tooltip.add(i + 1, new TranslatableComponent("jeidrawables.tooltip.liquid.fraction", fraction).withStyle(ChatFormatting.GRAY));
+						return;
+					}
+				}
+			}
+		};
 	}
 }
