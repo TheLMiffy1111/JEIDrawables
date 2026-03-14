33262c239a73d2b6468357be1f5544877a68355e
TheLMiffy1111 <6522061+TheLMiffy1111@users.noreply.github.com>
2026-03-14 17:38:06 +0800

    Backport colored texture drawables, add rotated drawable

--- src/main/java/thelm/jeidrawables/gui/render/GuiRenderUtil.java
+++ src/main/java/thelm/jeidrawables/gui/render/GuiRenderUtil.java
@@ -17,33 +17,41 @@ import net.minecraft.resources.ResourceLocation;
 public class GuiRenderUtil {
 
 	public static void blit(GuiGraphics guiGraphics, ResourceLocation atlasLocation, float x, float y, float uOffset, float vOffset, float width, float height, int textureWidth, int textureHeight) {
+		blit(guiGraphics, atlasLocation, x, y, uOffset, vOffset, width, height, textureWidth, textureHeight, -1);
+	}
+
+	public static void blitSprite(GuiGraphics guiGraphics, TextureAtlasSprite sprite, float x, float y, float uOffset, float vOffset, float width, float height, int textureWidth, int textureHeight) {
+		blitSprite(guiGraphics, sprite, x, y, uOffset, vOffset, width, height, textureHeight, textureHeight, -1);
+	}
+
+	public static void blit(GuiGraphics guiGraphics, ResourceLocation atlasLocation, float x, float y, float uOffset, float vOffset, float width, float height, int textureWidth, int textureHeight, int color) {
 		float uMin = uOffset / textureWidth;
 		float uMax = (uOffset + width) / textureWidth;
 		float vMin = vOffset / textureHeight;
 		float vMax = (vOffset + height) / textureHeight;
-		blit(guiGraphics, atlasLocation, x, x + width, y, y + height, uMin, uMax, vMin, vMax);
+		blit(guiGraphics, atlasLocation, x, x + width, y, y + height, uMin, uMax, vMin, vMax, color);
 	}
 
-	public static void blitSprite(GuiGraphics guiGraphics, TextureAtlasSprite sprite, float x, float y, float uOffset, float vOffset, float width, float height, int textureWidth, int textureHeight) {
+	public static void blitSprite(GuiGraphics guiGraphics, TextureAtlasSprite sprite, float x, float y, float uOffset, float vOffset, float width, float height, int textureWidth, int textureHeight, int color) {
 		float spriteWidth = sprite.getU1() - sprite.getU0();
 		float spriteHeight = sprite.getV1() - sprite.getV0();
 		float uMin = sprite.getU0() + uOffset / textureWidth * spriteWidth;
 		float uMax = sprite.getU0() + (uOffset + width) / textureWidth * spriteWidth;
 		float vMin = sprite.getV0() + vOffset / textureHeight * spriteHeight;
 		float vMax = sprite.getV0() + (vOffset + height) / textureHeight * spriteHeight;
-		blit(guiGraphics, sprite.atlasLocation(), x, x + width, y, y + height, uMin, uMax, vMin, vMax);
+		blit(guiGraphics, sprite.atlasLocation(), x, x + width, y, y + height, uMin, uMax, vMin, vMax, color);
 	}
 
-	static void blit(GuiGraphics guiGraphics, ResourceLocation atlasLocation, float xMin, float xMax, float yMin, float yMax, float uMin, float uMax, float vMin, float vMax) {
+	static void blit(GuiGraphics guiGraphics, ResourceLocation atlasLocation, float xMin, float xMax, float yMin, float yMax, float uMin, float uMax, float vMin, float vMax, int color) {
 		RenderSystem.setShaderTexture(0, atlasLocation);
-		RenderSystem.setShader(GameRenderer::getPositionTexShader);
+		RenderSystem.setShader(GameRenderer::getPositionTexColorShader);
 		Matrix4f matrix = guiGraphics.pose().last().pose();
 		BufferBuilder bufferBuilder = Tesselator.getInstance().getBuilder();
-		bufferBuilder.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.POSITION_TEX);
-		bufferBuilder.vertex(matrix, xMin, yMin, 0).uv(uMin, vMin).endVertex();
-		bufferBuilder.vertex(matrix, xMin, yMax, 0).uv(uMin, vMax).endVertex();
-		bufferBuilder.vertex(matrix, xMax, yMax, 0).uv(uMax, vMax).endVertex();
-		bufferBuilder.vertex(matrix, xMax, yMin, 0).uv(uMax, vMin).endVertex();
+		bufferBuilder.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.POSITION_COLOR_TEX);
+		bufferBuilder.vertex(matrix, xMin, yMin, 0).uv(uMin, vMin).color(color).endVertex();
+		bufferBuilder.vertex(matrix, xMin, yMax, 0).uv(uMin, vMax).color(color).endVertex();
+		bufferBuilder.vertex(matrix, xMax, yMax, 0).uv(uMax, vMax).color(color).endVertex();
+		bufferBuilder.vertex(matrix, xMax, yMin, 0).uv(uMax, vMin).color(color).endVertex();
 		BufferUploader.drawWithShader(bufferBuilder.end());
 	}
 }
