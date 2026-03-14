16891c12e189c4ccb21bbffab6236a9047f518e9
TheLMiffy1111 <6522061+TheLMiffy1111@users.noreply.github.com>
2026-03-15 05:15:46 +0800

    Add methods to create colored texture drawables from a base drawable

--- src/main/java/thelm/jeidrawables/gui/render/SpriteDrawable.java
+++ src/main/java/thelm/jeidrawables/gui/render/SpriteDrawable.java
@@ -5,16 +5,16 @@ import java.util.function.Supplier;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 
-public record SpriteDrawable(Supplier<TextureAtlasSprite> spriteSupplier, int u, int v, int width, int height, int textureWidth, int textureHeight, int color) implements IMaskableDrawable {
+public record SpriteDrawable(Supplier<TextureAtlasSprite> spriteSupplier, int u, int v, int width, int height, int textureWidth, int textureHeight, int color) implements ITextureDrawable {
 
 	public SpriteDrawable(Supplier<TextureAtlasSprite> spriteSupplier, int u, int v, int width, int height, int textureWidth, int textureHeight) {
 		this(spriteSupplier, u, v, width, height, textureWidth, textureHeight, -1);
 	}
-	
+
 	public SpriteDrawable(Supplier<TextureAtlasSprite> spriteSupplier, int textureWidth, int textureHeight, int color) {
 		this(spriteSupplier, 0, 0, textureWidth, textureHeight, textureWidth, textureHeight, color);
 	}
-	
+
 	public SpriteDrawable(Supplier<TextureAtlasSprite> spriteSupplier, int textureWidth, int textureHeight) {
 		this(spriteSupplier, 0, 0, textureWidth, textureHeight, textureWidth, textureHeight, -1);
 	}
@@ -37,12 +37,27 @@ public record SpriteDrawable(Supplier<TextureAtlasSprite> spriteSupplier, int u,
 	}
 
 	@Override
-	public IMaskableDrawable trim(int trimTop, int trimBottom, int trimLeft, int trimRight) {
+	public ITextureDrawable trim(int trimTop, int trimBottom, int trimLeft, int trimRight) {
 		int newWidth = Math.max(width - trimLeft - trimRight, 0);
 		int newHeight = Math.max(height - trimTop - trimBottom, 0);
 		if(newWidth == 0 || newHeight == 0) {
 			return new BlankDrawable(newWidth, newHeight);
 		}
 		return new SpriteDrawable(spriteSupplier, u + trimLeft, v + trimTop, newWidth, newHeight, textureWidth, textureHeight, color);
+	}
+
+	@Override
+	public ITextureDrawable withColor(int color) {
+		return new SpriteDrawable(spriteSupplier, u, v, width, height, textureWidth, textureHeight, 0xFF000000 & this.color | color);
+	}
+
+	@Override
+	public ITextureDrawable withAlpha(int alpha) {
+		return new SpriteDrawable(spriteSupplier, u, v, width, height, textureWidth, textureHeight, 0xFFFFFF & color | alpha << 24);
+	}
+
+	@Override
+	public ITextureDrawable withAlphaColor(int color) {
+		return new SpriteDrawable(spriteSupplier, u, v, width, height, textureWidth, textureHeight, color);
 	}
 }
