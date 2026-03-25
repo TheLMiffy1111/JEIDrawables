11b24ab4773d8bd657459b4849a926a159fa4625
TheLMiffy1111 <6522061+TheLMiffy1111@users.noreply.github.com>
2026-03-25 13:38:27 +0800

    Add blend func option to textured drawables

--- src/main/java/thelm/jeidrawables/gui/render/ResourceDrawable.java
+++ src/main/java/thelm/jeidrawables/gui/render/ResourceDrawable.java
@@ -4,18 +4,34 @@ import com.mojang.blaze3d.vertex.PoseStack;
 
 import net.minecraft.resources.ResourceLocation;
 
-public record ResourceDrawable(ResourceLocation atlasLocation, int u, int v, int width, int height, int textureWidth, int textureHeight, int color) implements ITextureDrawable {
+public record ResourceDrawable(ResourceLocation atlasLocation, int u, int v, int width, int height, int textureWidth, int textureHeight, int color, BlendFunction blendFunc) implements ITextureDrawable {
+
+	public ResourceDrawable(ResourceLocation atlasLocation, int u, int v, int width, int height, int textureWidth, int textureHeight, int color) {
+		this(atlasLocation, u, v, width, height, textureWidth, textureHeight, color, BlendFunction.TRANSLUCENT);
+	}
+
+	public ResourceDrawable(ResourceLocation atlasLocation, int u, int v, int width, int height, int textureWidth, int textureHeight, BlendFunction blendFunc) {
+		this(atlasLocation, u, v, width, height, textureWidth, textureHeight, -1, blendFunc);
+	}
 
 	public ResourceDrawable(ResourceLocation atlasLocation, int u, int v, int width, int height, int textureWidth, int textureHeight) {
-		this(atlasLocation, u, v, width, height, textureWidth, textureHeight, -1);
+		this(atlasLocation, u, v, width, height, textureWidth, textureHeight, -1, BlendFunction.TRANSLUCENT);
+	}
+
+	public ResourceDrawable(ResourceLocation atlasLocation, int u, int v, int width, int height, int color, BlendFunction blendFunc) {
+		this(atlasLocation, u, v, width, height, 256, 256, color, blendFunc);
 	}
 
 	public ResourceDrawable(ResourceLocation atlasLocation, int u, int v, int width, int height, int color) {
-		this(atlasLocation, u, v, width, height, 256, 256, color);
+		this(atlasLocation, u, v, width, height, 256, 256, color, BlendFunction.TRANSLUCENT);
+	}
+
+	public ResourceDrawable(ResourceLocation atlasLocation, int u, int v, int width, int height, BlendFunction blendFunc) {
+		this(atlasLocation, u, v, width, height, 256, 256, -1, blendFunc);
 	}
 
 	public ResourceDrawable(ResourceLocation atlasLocation, int u, int v, int width, int height) {
-		this(atlasLocation, u, v, width, height, 256, 256, -1);
+		this(atlasLocation, u, v, width, height, 256, 256, -1, BlendFunction.TRANSLUCENT);
 	}
 
 	@Override
@@ -31,7 +47,7 @@ public record ResourceDrawable(ResourceLocation atlasLocation, int u, int v, int
 	@Override
 	public void draw(PoseStack poseStack, float xOffset, float yOffset, float maskTop, float maskBottom, float maskLeft, float maskRight) {
 		if(maskLeft + maskRight < width && maskTop + maskBottom < height) {
-			GuiRenderUtil.blit(poseStack, atlasLocation, xOffset + maskLeft, yOffset + maskTop, u + maskLeft, v + maskTop, width - maskLeft - maskRight, height - maskTop - maskBottom, textureWidth, textureHeight, color);
+			GuiRenderUtil.blit(poseStack, atlasLocation, xOffset + maskLeft, yOffset + maskTop, u + maskLeft, v + maskTop, width - maskLeft - maskRight, height - maskTop - maskBottom, textureWidth, textureHeight, color, blendFunc);
 		}
 	}
 
@@ -42,21 +58,21 @@ public record ResourceDrawable(ResourceLocation atlasLocation, int u, int v, int
 		if(newWidth == 0 || newHeight == 0) {
 			return new BlankDrawable(newWidth, newHeight);
 		}
-		return new ResourceDrawable(atlasLocation, u + trimLeft, v + trimTop, newWidth, newHeight, textureWidth, textureHeight, color);
+		return new ResourceDrawable(atlasLocation, u + trimLeft, v + trimTop, newWidth, newHeight, textureWidth, textureHeight, color, blendFunc);
 	}
 
 	@Override
 	public ITextureDrawable withColor(int color) {
-		return new ResourceDrawable(atlasLocation, u, v, width, height, textureWidth, textureHeight, 0xFF000000 & this.color | color);
+		return new ResourceDrawable(atlasLocation, u, v, width, height, textureWidth, textureHeight, 0xFF000000 & this.color | color, blendFunc);
 	}
 
 	@Override
 	public ITextureDrawable withAlpha(int alpha) {
-		return new ResourceDrawable(atlasLocation, u, v, width, height, textureWidth, textureHeight, 0xFFFFFF & color | alpha << 24);
+		return new ResourceDrawable(atlasLocation, u, v, width, height, textureWidth, textureHeight, 0xFFFFFF & color | alpha << 24, blendFunc);
 	}
 
 	@Override
 	public ITextureDrawable withAlphaColor(int color) {
-		return new ResourceDrawable(atlasLocation, u, v, width, height, textureWidth, textureHeight, color);
+		return new ResourceDrawable(atlasLocation, u, v, width, height, textureWidth, textureHeight, color, blendFunc);
 	}
 }
